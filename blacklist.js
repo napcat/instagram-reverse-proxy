574ebfe4bcd3e5749deea094503c9d8b6be5c999
napcat <patcruzmail@gmail.com>
2017-09-12 11:12:54 +0100

    Update blacklist.js

--- blacklist.js
+++ blacklist.js
@@ -1,5 +1,4 @@
 exports.list = [
-  'batalhatemfuturoautarquicas2017.pt',
   'bloodandco.com',
   'bnk48.com',
   'centraldacorrida.com.br',
@@ -9,8 +8,6 @@ exports.list = [
   'leonidasoy.fi',
   'likedike.com',
   'likes.my',
-  'mplaeleicoes2017.com',
-  'ofuturocomcerteza.pt',
   'potomacfloralwholesale.com',
   'pressingprive.com',
   'weekendtrip.fr'
